8264c11d5cb742af3e7c98689380542e636e79b5
RuslanMysh <ruslanmyshkovskij@gmail.com>
2025-01-27 18:27:51 +0700

    Добавлен label

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -29,32 +29,45 @@
         private void InitializeComponent()
         {
             pictureBox1 = new PictureBox();
+            TimeTableLabel = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.AntiqueWhite;
-            pictureBox1.Location = new Point(656, 12);
+            pictureBox1.Location = new Point(12, 332);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(132, 141);
+            pictureBox1.Size = new Size(776, 106);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            // 
+            // TimeTableLabel
+            // 
+            TimeTableLabel.AutoSize = true;
+            TimeTableLabel.Location = new Point(12, 9);
+            TimeTableLabel.Name = "TimeTableLabel";
+            TimeTableLabel.Size = new Size(78, 15);
+            TimeTableLabel.TabIndex = 2;
+            TimeTableLabel.Text = "Расписание: ";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(TimeTableLabel);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label TimeTableLabel;
     }
 }
