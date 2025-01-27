d4ab1eeb20795da4448391a99a3eacd58468fb02
мк <germanvasanov@gmail.com>
2025-01-27 20:12:58 +0700

    тест первых картинок

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -28,19 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
             TimeTableLabel = new Label();
             button1 = new Button();
             label1 = new Label();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.AntiqueWhite;
-            pictureBox1.Location = new Point(12, 285);
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(388, 40);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(776, 106);
+            pictureBox1.Size = new Size(400, 398);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -74,11 +79,22 @@
             label1.TabIndex = 4;
             label1.Text = "Расписание: ";
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 40);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(380, 398);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(TimeTableLabel);
@@ -86,6 +102,7 @@
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,5 +113,6 @@
         private Label TimeTableLabel;
         private Button button1;
         private Label label1;
+        private PictureBox pictureBox2;
     }
 }
