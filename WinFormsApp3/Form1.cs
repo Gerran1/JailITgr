3d3fd8e9e3e0b78dfd21819b82c56526cf35ac83
RuslanMysh <ruslanmyshkovskij@gmail.com>
2025-01-27 20:22:01 +0700

    Готовый тест

--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -7,7 +7,7 @@ namespace WinFormsApp3
         public Form1()
         {
             InitializeComponent();
-            StartTest();
+            Test();
             TimeTableLabel.Text = TimeTables[0];
         }
 
@@ -16,7 +16,7 @@ namespace WinFormsApp3
             switch (i)
             {
                 case 0:
-
+                    //подъём
                 break;
             }
             if (i != 8)
@@ -31,9 +31,55 @@ namespace WinFormsApp3
             }
            
         }
-        public void StartTest()
+        public void Test()
         {
-            
+            int testPoints = 0;
+
+           DialogResult dr = MessageBox.Show("Опыт в кодинге более 3-х лет?", "Первый вопрос", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                testPoints += 2;
+            }
+            else
+            {
+                testPoints += 1;
+            }
+
+            dr = MessageBox.Show("Паттерны использовал?", "Второй вопрос", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                testPoints += 2;
+            }
+            else
+            {
+                testPoints += 1;
+            }
+
+            dr = MessageBox.Show("Чужой код воровал", "Третий вопрос?", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                testPoints += 1;
+            }
+            else
+            {
+                testPoints += 2;
+            }
+
+            if (testPoints == 6)
+            {
+                MessageBox.Show("Ну тут всё понятно, это Lua");
+            }
+            else if (testPoints >= 4 && testPoints < 6)
+            {
+                MessageBox.Show("Вот ты и попался питонист!");
+            }
+            else if (testPoints == 3)
+            {
+                MessageBox.Show("Ооооо, плюсы тебя с костями съедят, паскальный!");
+            }
         }
     }
 }
