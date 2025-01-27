dcf47793a8deff1d3e3d234ef9ff67214b8415a9
RuslanMysh <ruslanmyshkovskij@gmail.com>
2025-01-27 18:53:06 +0700

    Первый функционал кнопки "Далее"

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -31,6 +31,7 @@
             pictureBox1 = new PictureBox();
             TimeTableLabel = new Label();
             button1 = new Button();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -46,11 +47,11 @@
             // TimeTableLabel
             // 
             TimeTableLabel.AutoSize = true;
-            TimeTableLabel.Location = new Point(12, 9);
+            TimeTableLabel.Location = new Point(82, 9);
             TimeTableLabel.Name = "TimeTableLabel";
-            TimeTableLabel.Size = new Size(78, 15);
+            TimeTableLabel.Size = new Size(12, 15);
             TimeTableLabel.TabIndex = 2;
-            TimeTableLabel.Text = "Расписание: ";
+            TimeTableLabel.Text = "-";
             // 
             // button1
             // 
@@ -62,12 +63,23 @@
             button1.TabIndex = 3;
             button1.Text = " Далее";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(-2, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Расписание: ";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(TimeTableLabel);
             Controls.Add(pictureBox1);
@@ -83,5 +95,6 @@
         private PictureBox pictureBox1;
         private Label TimeTableLabel;
         private Button button1;
+        private Label label1;
     }
 }
--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -2,9 +2,27 @@ namespace WinFormsApp3
 {
     public partial class Form1 : Form
     {
+        int i = 0;
+        List<string> TimeTables = new List<string>() { "Подъём", "Завтрак", "Перекличка", "Работа", "Обед", "Cвободное Время", "Ужин", "Подготовка ко сну", "Сон" };
         public Form1()
         {
             InitializeComponent();
+            TimeTableLabel.Text = TimeTables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i != 8)
+            {
+                i++;
+                TimeTableLabel.Text = TimeTables[i];
+            }
+            else
+            {
+                i = 0;
+                TimeTableLabel.Text = TimeTables[i];
+            }
+            
         }
     }
 }
